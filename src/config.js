6bf77ff64c9b8a0761b0534ec333606fbf470559
Jvymvn <Jordon.carter@yahoo.com>
2019-10-09 23:28:05 -0400

    Seed adjustment

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DATABASE_URL || 'postgresql://Jordon@localhost/things-to-trade',
-    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    JWT_SECRET: process.env.JWT_SECRET || 'trade_token',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
 }
--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const UsersService = require('./users-service')
-
 const usersRouter = express.Router()
 const jsonBodyParser = express.json()
 
